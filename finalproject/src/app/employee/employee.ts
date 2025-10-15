6424e82b68b29830b92bf91583fbf6e2700df91b
Polawat Krates <fuefolkjj1230@gmail.com>
2025-10-15 13:41:17 +0700

    Graph

--- finalproject/src/app/employee/employee.ts
+++ finalproject/src/app/employee/employee.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-employee',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './employee.html',
   styleUrl: './employee.css'
 })
