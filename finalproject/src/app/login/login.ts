223c4a61afbe58d545b1a0661e9e743132d2f142
Baramee Maprom <BarameeMaprom@gmail.com>
2025-10-15 12:40:50 +0700

    ล็อกอิน

--- finalproject/src/app/login/login.ts
+++ finalproject/src/app/login/login.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -7,6 +8,45 @@ import { RouterLink } from '@angular/router';
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
-export class Login {
+
+export class Login implements OnInit {
+  signupUsers : any[] = [];
+
+  signupObj : any = {
+    username : '',
+    email : '',
+    password : ''
+  };
+
+  loginObj : any = {
+    email : '',
+    password : ''
+  };
+
+  ngOnInit(): void {
+      const localData = localStorage.getItem('signupUsers');
+      if(localData != null) {
+        this.signupUsers = JSON.parse(localData);
+      }
+  }
+
+  onSignup() {
+    this.signupUsers.push(this.signupObj);
+    localStorage.setItem('signupUsers', JSON.stringify(this.signupUsers));
+    this.signupObj = {
+      username : '',
+      email : '',
+      password : ''
+    };
+  }
+
+  onLogin() {
+    const isUserExist = this.signupUsers.find(m => m.email == this.loginObj.email && m.password == this.loginObj.password);
+    if(isUserExist != undefined) {
+      alert('Login Successful');
+    } else {
+      alert('Invalid Credentials');
+    }
+  }
 
 }
