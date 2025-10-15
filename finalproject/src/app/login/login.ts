9f8dfc591118f6099239b1d9cb20d6ab4625e743
Baramee Maprom <BarameeMaprom@gmail.com>
2025-10-15 14:03:39 +0700

    login successfully

--- finalproject/src/app/login/login.ts
+++ finalproject/src/app/login/login.ts
@@ -2,19 +2,65 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink],
+  imports: [FormsModule, RouterLink],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
-export class Login {
+export class Login implements OnInit {
+
+  constructor(private router: Router) {}
+
   isActive = false;
   toggleRegister(){
     this.isActive = true;
   }
   toggleUser() {
     this.isActive = false;
+  }
+
+  signupUsers : any[] = [];
+
+  signupObj : any = {
+    username : '',
+    email : '',
+    password : ''
+  };
+
+  loginObj : any = {
+    email : '',
+    password : ''
+  };
+
+  ngOnInit(): void {
+      const localData = localStorage.getItem('signupUsers');
+      if(localData != null) {
+        this.signupUsers = JSON.parse(localData);
+      }
+  }
+
+  onSignup() {
+    this.signupUsers.push(this.signupObj);
+    localStorage.setItem('signupUsers', JSON.stringify(this.signupUsers));
+    this.signupObj = {
+      username : '',
+      email : '',
+      password : ''
+    };
+
+    alert('Register Successful');
+  }
+
+  onLogin() {
+    const isUserExist = this.signupUsers.find(m => m.email == this.loginObj.email && m.password == this.loginObj.password);
+    if(isUserExist != undefined) {
+      alert('Login Successful');
+      this.router.navigate(['/Dashboard']);
+    } else {
+      alert('Invalid Credentials');
+    }
   }
 }
