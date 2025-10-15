18a94ae8573bcc8554782d9509d5e88214f85f8e
Polawat Krates <fuefolkjj1230@gmail.com>
2025-10-15 12:11:45 +0700

    Login component

--- finalproject/src/app/app.routes.ts
+++ finalproject/src/app/app.routes.ts
@@ -1,19 +1,16 @@
 import { Routes } from '@angular/router';
-import { Dashboard } from './dashboard/dashboard';
-import { Employee } from './employee/employee';
 
 export const routes: Routes = [
     {
-        path:'',
-        redirectTo: 'Dashboard',
-        pathMatch:'full'
-    },
-    {
-        path:'Dashboard',
-        component:Dashboard
-    },
-    {
-        path:'Employee',
-        component:Employee
-    }
+    path: '',
+    loadChildren: () => import('./login/login.routes').then(m => m.LOGIN_ROUTES)
+  },
+  {
+    path: 'Employee',
+    loadChildren: () => import('./employee/employee.routes').then(m => m.EMPLOYEE_ROUTES)
+  },
+  {
+    path: 'Dashboard',
+    loadChildren: () => import('./dashboard/dashboard.routes').then(m => m.DASHBOARD_ROUTES)
+  }
 ];
---
+++ finalproject/src/app/employee/employee.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+import { Employee } from './employee';
+
+export const EMPLOYEE_ROUTES: Routes = [
+  { path: '', 
+    component: Employee }
+];
